323f36ae05253921949aec842c3d46eac34b6e82
Bahrom <bahrom.matyakubov@gmail.com>
2017-01-14 15:13:16 -0500

    Updated tag and renamed the package -  got it mixed up with root dir

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='answeroid',
     packages=['answeroid'],
-    version='0.4',
+    version='0.5',
     description='A simple bot for helping on Q/A sites',
     author='Bahrom Matyakubov',
     author_email='bahrom.matyakubov@gmail.com',
     url='https://github.com/bahrom-matyakubov/answeroid',
-    download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.4',
+    download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.5',
     keywords=['Q&A', 'answer', 'question', 'help', 'bot'],
     classifiers=[],
 )
