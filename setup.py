45ddc36343047460dc38dd863a0e130f02cd7e90
Bahrom <bahrom.matyakubov@gmail.com>
2017-01-14 15:06:58 -0500

    Had to add my email because of missing meta-data pypi warning

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.3',
     description='A simple bot for helping on Q/A sites',
     author='Bahrom Matyakubov',
+    author_email='bahrom.matyakubov@gmail.com',
     url='https://github.com/bahrom-matyakubov/answeroid',
     download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.3',
     keywords=['Q&A', 'answer', 'question', 'help', 'bot'],
