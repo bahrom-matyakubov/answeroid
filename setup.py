5a9f641b43ec26f6885d9e37ae65ada5efed85ae
Bahrom <bahrom.matyakubov@gmail.com>
2017-01-14 15:04:01 -0500

    Updating the tags in setup.py (there should really be a better way of updating tags, investigate)

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name='answeroid',
     packages=['answeroid'],
-    version='0.1',
+    version='0.3',
     description='A simple bot for helping on Q/A sites',
     author='Bahrom Matyakubov',
     url='https://github.com/bahrom-matyakubov/answeroid',
-    download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.1',
+    download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.3',
     keywords=['Q&A', 'answer', 'question', 'help', 'bot'],
     classifiers=[],
 )
