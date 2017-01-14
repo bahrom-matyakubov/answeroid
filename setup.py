95ca35ebd9a0cfb95fd1168ebe9a2febc5f586b8
Bahrom <bahrom.matyakubov@gmail.com>
2017-01-14 15:00:01 -0500

    Updated setup.py (http://peterdowns.com/posts/first-time-with-pypi.html)

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='answeroid',
+    packages=['answeroid'],
+    version='0.1',
+    description='A simple bot for helping on Q/A sites',
+    author='Bahrom Matyakubov',
+    url='https://github.com/bahrom-matyakubov/answeroid',
+    download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.1',
+    keywords=['Q&A', 'answer', 'question', 'help', 'bot'],
+    classifiers=[],
+)
