5feb3795d53a16321d4bdf47e34d47cbf455d090
Bahrom <bahrom.matyakubov@gmail.com>
2017-01-15 00:18:18 -0500

    Added pypi instructions

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 
+# To register (first time):
+# python setup.py register -r pypi
+# To upload, create a new tag (git tag version, git push --tags origin master), then run:
+# python setup.py sdist upload -r pypi
+
 setup(
     name='answeroid',
     packages=['answeroid', 'helpers', 'sites'],
