56c5726d9a73feaa392bf7b5153696547b846e07
Bahrom <bahrom.matyakubov@gmail.com>
2017-01-15 02:51:06 -0500

    Updated tags, install reqs

--- setup.py
+++ setup.py
@@ -8,17 +8,18 @@ from distutils.core import setup
 setup(
     name='answeroid',
     packages=['answeroid', 'helpers', 'sites'],
-    version='0.6',
+    version='0.7',
     description='A simple bot for helping on Q/A sites',
     author='Bahrom Matyakubov',
     author_email='bahrom.matyakubov@gmail.com',
     url='https://github.com/bahrom-matyakubov/answeroid',
-    download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.6',
+    download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.7',
     keywords=['Q&A', 'answer', 'question', 'help', 'bot'],
     classifiers=[],
     install_requires=[
         'bs4',
         'requests',
-        'wolframalpha'
+        'wolframalpha',
+        'sympy'
     ],
 )
